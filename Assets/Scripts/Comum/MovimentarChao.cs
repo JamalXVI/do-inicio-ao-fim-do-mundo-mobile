bdb6a931be9a443c5f483de501ded66204cc82b1
Henrique Arantes Tiraboschi <henrique_arantes_338@hotmail.com>
2019-01-09 23:08:11 -0200

    Initializing the Movement Script
    Initializing the Movement for the  platatform

---
+++ Assets/Scripts/Comum/MovimentarChao.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Comum.Chao
+{
+    public class MovimentarChao : MonoBehaviour
+    {
+        [FormerlySerializedAs("Velocidade")]
+        [SerializeField]
+        private float _velocidade;
+
+        public Propriedades Propriedades { get; private set; }
+
+        public float Velocidade
+        {
+            get
+            {
+                return _velocidade;
+            }
+        }
+
+
+        private void Awake()
+        {
+            float tamanhoDaImagem = this.GetComponent<SpriteRenderer>().size.x;
+            float escala = this.transform.localScale.x;
+            this.Propriedades = new Propriedades(this.transform.position, tamanhoDaImagem * escala);
+        }
+        void Update()
+        {
+            float deslocamento = Mathf.Repeat(this.Velocidade * Time.time, this.Propriedades.TamanhoImagemReal);
+            this.transform.position = this.Propriedades.PosicaoInicial + Vector3.left * deslocamento;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Comum/Propriedades.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Comum.Chao
+{
+    public class Propriedades
+    {
+        private readonly Vector3 _posicaoInicial;
+        public Vector3 PosicaoInicial
+        {
+            get
+            {
+                return this._posicaoInicial;
+            }
+        }
+        private readonly float _tamanhoImagemReal;
+        public float TamanhoImagemReal
+        {
+            get
+            {
+                return this._tamanhoImagemReal;
+            }
+        }
+        public Propriedades(Vector3 posicaoInicial, float tamanhoImagemReal)
+        {
+            this._posicaoInicial = posicaoInicial;
+            this._tamanhoImagemReal = tamanhoImagemReal;
+        }
+    }
+}
